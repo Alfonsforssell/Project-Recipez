8709d109f34ecc78f857e8410a55e39aa294e78d
Rickardaker <rickard.akerfeldt@hotmail.com>
2026-05-12 16:19:37 +0200

    klar med getDietById

--- assets/JS/Backend/dietery.js
+++ assets/JS/Backend/dietery.js
@@ -1,8 +1,8 @@
-export function getAllDiets () {
+export function getAllDiets() {
     const text = Deno.readTextFileSync("data.json");
     const data = JSON.parse(text);
     let diets = data.dietary;
-    
+
     return new Response(JSON.stringify(diets), {
         status: 200,
         headers: {
@@ -14,19 +14,36 @@ export function getAllDiets () {
     });
 }
 
-export function getDietById (id) {
+export function getDietById(id) {
+    const text = Deno.readTextFileSync("data.json");
+    const data = JSON.parse(text);
+    let diets = data.dietary;
 
+    for (let diet of diets) {
+        if (diet.id == id) {
+            return new Response(JSON.stringify(diet), {
+                status: 200,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+                    "Access-Control-Allow-Headers": "Content-Type, Authorization"
+                }
+            });
+
+        }
+    }
 }
 
 export function SearchDiet(request) {
 
 }
-export function createDiet (request) {
+export function createDiet(request) {
 
 }
-export function patchDiet (reqest, id) {
+export function patchDiet(reqest, id) {
 
-} 
-export function deleteDiet (id) {
+}
+export function deleteDiet(id) {
 
 }
