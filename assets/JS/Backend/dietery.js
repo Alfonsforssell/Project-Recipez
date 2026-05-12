60f34140431fed95a1bb2e9a16aaf3d9fa0c8da2
Rickardaker <rickard.akerfeldt@hotmail.com>
2026-05-12 16:16:31 +0200

    Finished getAllDiets

--- assets/JS/Backend/dietery.js
+++ assets/JS/Backend/dietery.js
@@ -1,5 +1,17 @@
 export function getAllDiets () {
-
+    const text = Deno.readTextFileSync("data.json");
+    const data = JSON.parse(text);
+    let diets = data.dietary;
+    
+    return new Response(JSON.stringify(diets), {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization"
+        }
+    });
 }
 
 export function getDietById (id) {
