76dc6fb336331103eae04e5f7732382b040cacea
Rickardaker <rickard.akerfeldt@hotmail.com>
2026-05-13 15:16:41 +0200

    added delete

--- assets/JS/Backend/server.js
+++ assets/JS/Backend/server.js
@@ -1,7 +1,7 @@
 import * as diets from "./dietrys.js";
 import * as users from "./users.js";
 import * as dishes from "./dishes.js";
-import { serveDir } from "jsr:@std/http/file-server"; 
+import { serveDir } from "jsr:@std/http/file-server";
 
 function validateJsonContent(request) {
     let content = request.headers.get("Content-Type");
@@ -20,32 +20,45 @@ const HEADERS = {
     "Access-Control-Allow-Headers": "Content-Type, Authorization"
 };
 
-async function handler (request) {
+async function handler(request) {
     let url = new URL(request.url);
-    let dishIdRoute = new URLPattern({ pathname: "dishes/:id" }); 
-    let dietaryIdRoute = new URLPattern({ pathname: "dietary/:id" }); 
+    let dishIdRoute = new URLPattern({ pathname: "dishes/:id" });
+    let dietaryIdRoute = new URLPattern({ pathname: "dietary/:id" });
     let dietaryDishesRoute = new URLPattern({ pathname: "dietary/:id/dishes" });
     let userIdRoute = new URLPattern({ pathname: "users/:id" });
     let userFavoritesRoute = new URLPattern({ pathname: "users/:id/favorites" });
+    let dishIdMatch = dishIdRoute.exec(url);
 
     if (request.method === "GET") {
-        
+
     }
 
     if (request.method === "POST") {
 
     }
 
-    if (request.method === "DELETE") {
-        /* tjo deleta mig 
-        snälla 
-        ha ha ha
-        */
 
+    if (request.method === "DELETE") {
+        if (dishIdMatch) {
+            let id = dishIdMatch.pathname.groups.id;
+            let deletedDish = dishes.deleteDish(id);
+
+            if (!deletedDish) {
+                return new Response(JSON.stringify({ error: "Dish not found" }), {
+                    status: 404,
+                    headers: HEADERS
+                });
+            }
+            return new Response(JSON.stringify(deletedDish), {
+                status: 200,
+                headers: HEADERS
+            });
+        }
     }
 
     if (request.method === "PATCH") {
 
+        
     }
 }
 
