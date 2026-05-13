9e0fa4858ffb82cd9f477ed5ff99b18342c34ec0
Rickardaker <rickard.akerfeldt@hotmail.com>
2026-05-13 14:14:15 +0200

    Update dietery.js

--- assets/JS/Backend/dietery.js
+++ assets/JS/Backend/dietery.js
@@ -3,15 +3,7 @@ export function getAllDiets() {
     const data = JSON.parse(text);
     let diets = data.dietary;
 
-    return new Response(JSON.stringify(diets), {
-        status: 200,
-        headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization"
-        }
-    });
+    return diets;
 }
 
 export function getDietById(id) {
@@ -22,54 +14,8 @@ export function getDietById(id) {
 
     for (let diet of diets) {
         if (diet.id == id) {
-           let matchedDiet = diet; 
+           matchedDiet = diet; 
         }
     }
    return matchedDiet;
 }
-/*
-export function SearchDiet(request) {
-    const text = Deno.readTextFileSync("data.json");
-    const data = JSON.parse(text);
-    let diets = data.dietary;
-    let url = new URL(request.url);
-
-    let q = url.searchParams.get("q");
-    let result = [];
-
-    if (!q) return new Response("bad request", {
-        status: 400,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization"
-        }
-    });
-
-    let search = q.toLowerCase();
-    
-    for (let diet of diets) {
-        if (diet.name.toLowerCase().includes(search)) {
-            result.push(diet);
-        }
-    }
-
-     return new Response(JSON.stringify(result), {
-        status: 200,
-        headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization"
-        }
-    });
-}
-export function findDishByDietId (id, request) {
-    const text = Deno.readTextFileSync("data.json");
-    const data = JSON.parse(text);
-    let diets = data.dietary;
-    let url = new URL(request.url);
-    
-    
-}
-*/
