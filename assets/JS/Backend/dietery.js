fb4a8f7cd17df4ee80aebb3833846f6836713078
Oggylito <thordeneddie@gmail.com>
2026-05-14 15:19:43 +0200

    Fixed getDishesByDietId function

--- assets/JS/Backend/dietery.js
+++ assets/JS/Backend/dietery.js
@@ -20,11 +20,17 @@ export function getDietById(id) {
     return matchedDiet;
 }
 
-/*
-export function getDishesByDietId(id, request) {
+export function getDishesByDietId(id) {
     const text = Deno.readTextFileSync("data.json");
     const data = JSON.parse(text);
-    let diets = data.dietary;
-    let dishes = data.dishes
+    let dishes = data.dishes;
+    let matchedDishes = [];
+     
+    for (let dish of dishes) {
+        if (dish.id === id) {
+            matchedDishes.push(dish); 
+        }
+    }
+
+    return matchedDishes;
 }
-*/
